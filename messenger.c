f850cae33ddc64054caa75279ab55a6a8f7da867
Adam Kosicki <100010326+Adam-Kosicki@users.noreply.github.com>
2023-08-07 16:15:01 -0500

    Update messenger.c

--- messenger.c
+++ messenger.c
@@ -4,37 +4,40 @@
 
 #define MAXLEN 1024
 
-FILE *finput, *foutput;
+FILE *finput, *foutput; // File pointers for input and output files
+char *argv1; // Stores the command-line argument for read-pipe
 
-char *argv1;
-
+// Function executed by the thread to read from a pipe and print its contents
 void *pipe_read_thread(void *ptr) {
 	char temp[MAXLEN];
-	finput = fopen(argv1, "r");
-	while (fgets(temp, MAXLEN, finput))
-			puts(temp);
+	finput = fopen(argv1, "r"); // Open the specified read-pipe
+	while (fgets(temp, MAXLEN, finput)) // Read line by line from the file
+		puts(temp); // Print each line
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
+	if (argc != 3) { // Check if the correct number of command-line arguments is provided
 		fprintf(stderr, "Usage: messenger <read-pipe> <write-pipe>");
 		exit(1);
 	}
 	
-	argv1 = argv[1];
+	argv1 = argv[1]; // Store the read-pipe filename from the command-line argument
 	
-	pthread_t thread1;
+	pthread_t thread1; // Thread variable
+	
+	// Create a thread to read from the read-pipe
 	if (pthread_create(&thread1, NULL, pipe_read_thread, NULL)) {
 		fputs("Cannot create thread.\n", stderr);
 		exit(1);
 	}
 	
-	foutput = fopen(argv[2], "w");
+	foutput = fopen(argv[2], "w"); // Open the specified write-pipe for output
 	
-	char buffer[MAXLEN];
+	char buffer[MAXLEN]; // Buffer to store input data
 	
+	// Read from standard input and write to the write-pipe
 	while (fgets(buffer, MAXLEN, stdin)) {
-		fputs(buffer, foutput);
-		fflush(foutput);
+		fputs(buffer, foutput); // Write data to the write-pipe
+		fflush(foutput); // Flush the output stream
 	}
 }
